234aae035bec11daced847feef690eb6955a3848
pachas7 <josedavid.b77@gmail.com>
2018-11-20 16:47:41 +0100

    Update eip.tf

--- eip.tf
+++ eip.tf
@@ -1,7 +1,8 @@
 
-resource "aws_eip" "eip"{
-}
-resource "aws_eip_association" "eip_assoc"{
-  instance_id = "${aws_instance.web.id}"
-  allocation_id = "${aws_eip.eip.id}"
-}
+#resource "aws_eip" "eip"{
+#}
+
+#resource "aws_eip_association" "eip_assoc"{
+#  instance_id = "${aws_instance.web.id}"
+#  allocation_id = "${aws_eip.eip.id}"
+#}
