c88e5e0609d4bf4bdb7b0bf3f29963ce09bc6311
pachas7 <josedavid.b77@gmail.com>
2018-11-21 12:42:54 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -30,3 +30,19 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_ssh"
   }
 }
+
+resource "aws_security_group" "allow_outbound_all" {
+  name        = "allow_outbound_all"
+  description = "Allow all outbound traffic"
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_outbound_all"
+  }
+}
