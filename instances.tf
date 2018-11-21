063c396d34d841e9bb8b64e07b5b35d7b1b8ca33
pachas7 <josedavid.b77@gmail.com>
2018-11-21 12:43:23 +0100

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"  
   user_data = "${data.template_file.user_data.rendered}"
   key_name = "Personal-JD"
-  vpc_security_group_ids = [ "${aws_security_group.allow_http.id}" , "${aws_security_group.allow_ssh.id}" ]
+  vpc_security_group_ids = [ "${aws_security_group.allow_http.id}" , "${aws_security_group.allow_ssh.id}" , "${aws_security_group.allow_outbound_all.id}" ]
   tags {
     Name = "TerraformLabs"
   }
