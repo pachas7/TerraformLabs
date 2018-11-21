56c7fd4cf78257847f1e31d29c90862478d20f7a
pachas7 <josedavid.b77@gmail.com>
2018-11-21 11:57:14 +0100

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -16,7 +16,8 @@ resource "aws_instance" "web" {
   ami = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"  
   user_data = "${data.template_file.user_data.rendered}"
-  security_groups = "${aws_security_group.allow_http.id}"
+  key_name = "Personal-JD"
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
   tags {
     name = "PuppetMaster"
   }
