e74e9f23b37799a38ac59a2c184d692b460d7846
pachas7 <josedavid.b77@gmail.com>
2018-11-21 10:59:55 +0100

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "web" {
   ami = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"  
   user_data = "${data.template_file.user_data.rendered}"
-  
+  security_groups = "${aws_security_group.allow_http.id}"
   tags {
     name = "PuppetMaster"
   }
