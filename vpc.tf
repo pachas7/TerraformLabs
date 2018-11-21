402fa491851034224d93d85ba853b27d05b38380
pachas7 <josedavid.b77@gmail.com>
2018-11-21 12:20:50 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -13,3 +13,20 @@ resource "aws_security_group" "allow_http" {
     Name = "allow_http"
   }
 }
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_ssh"
+  }
+}
