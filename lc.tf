cd2d428da6f4fa8e3d54b906558c6665f1260874
pachas7 <josedavid.b77@gmail.com>
2018-11-22 16:23:07 +0100

    Update lc.tf

--- lc.tf
+++ lc.tf
@@ -1,5 +1,8 @@
 resource "aws_launch_configuration" "web" {
-  name          = "${var.project_name}-lc"
+  name_prefix          = "${var.project_name}-lc"
   image_id      = "${data.aws_ami.ubuntu.id}"
   instance_type = "${var.instance_type}"
+  key_name = "Personal-JD"
+  security_groups = [ "${aws_security_group.allow_http.id}" , "${aws_security_group.allow_ssh.id}" , "${aws_security_group.allow_outbound_all.id}" ]
+  user_data = "${data.template_file.user_data.rendered}"
 }
