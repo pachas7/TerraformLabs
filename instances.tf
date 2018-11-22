596f90f05f9ef9368494d9b57f4b4ccae7d0f807
pachas7 <josedavid.b77@gmail.com>
2018-11-22 14:08:24 +0100

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"  
+  instance_type = "${var.instance_type}"  
   user_data = "${data.template_file.user_data.rendered}"
   key_name = "Personal-JD"
   vpc_security_group_ids = [ "${aws_security_group.allow_http.id}" , "${aws_security_group.allow_ssh.id}" , "${aws_security_group.allow_outbound_all.id}" ]
