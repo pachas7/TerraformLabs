55554960fb3bc6d70cf2891510cbc05e3b9a8eed
pachas7 <josedavid.b77@gmail.com>
2018-11-23 15:54:02 +0100

    Update init.sh

--- Docker/init.sh
+++ Docker/init.sh
@@ -6,6 +6,7 @@ function start {
   docker build -t terraformlabs .
   docker run --name terraformlabs -d -t terraformlabs
   docker exec -it terraformlabs /bin/sh -c 'cd /opt/terraformlabs/ ; terraform init ; terraform apply -var-file=variables.tfvars -var "webpage=$webpage"'
+}
 
 read -p "Did you write the AWS_access in variables.tfvars (y/n)?" choice
 case "$choice" in 
