d53a828a13381ab1a39e9ed1ab289d8ed3453601
pachas7 <josedavid.b77@gmail.com>
2018-11-23 16:03:12 +0100

    Update init.sh

--- Docker/init.sh
+++ Docker/init.sh
@@ -2,7 +2,7 @@
 
 function start {
   read -p "What do you want to show in your website?" webpage
-  echo -e "\nwebpage=$webpage" >> variables.tfvars
+  echo -e '\nwebpage="$webpage"' >> variables.tfvars
   docker build -t terraformlabs .
   docker run --name terraformlabs -d -t terraformlabs
   docker exec -it terraformlabs /bin/sh -c 'cd /opt/terraformlabs/ ; terraform init ; terraform apply -var-file=variables.tfvars'
